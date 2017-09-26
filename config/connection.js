51bc5dab49d79f919d10c043eff88c4aad8e80cb
Muddavugga <dennisalvey423@gmail.com>
2017-09-26 14:17:58 -0700

    adding connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "muddavugga",
-  password: "Posies23",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) { // if you are using clearDB - you will use the cleardb url - see the config vars
+  // We are in production
+  connection= mysql.createConnection({
+      host: process.env.HOST,
+      user: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE_NAME
+  });
+} else {
+  // We are in a local environment
+  connection = mysql.createConnection({
+      host: "localhost",
+      user: "muddavugga",
+      password: "Posies23",
+      database: "burgers_db"
+  });
+}; 
 
 // Export connection for our ORM to use.
 module.exports = connection;
