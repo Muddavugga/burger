d592b2128026e6dc1a5226df31e1c42e252c4b08
Dennis <dennisalvey423@gmail.com>
2017-11-01 16:19:11 -0700

    burger project

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@
 -- date: a TIMESTAMP.
 
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
 
 CREATE TABLE burgers(
     id INTEGER AUTO_INCREMENT NOT NULL,
